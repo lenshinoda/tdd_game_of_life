5de4665331c0b123bbca7adaf810b966cc198e83
lenshinoda <lebps1996@gmail.com>
2021-06-23 08:48:00 -0500

    [test-grid] Given a cell location the neighbors should be calculated - pass

--- game/grid.py
+++ game/grid.py
@@ -5,3 +5,18 @@ class Grid:
     def __init__(self, seed):
         self.seed = seed
 
+    def calculate_neighbors(self, location):
+        x = location[0]
+        y = location[1]
+
+        neighbors = 0 - self.seed[y][x]
+
+        for i in [-1, 0, 1]:
+            for j in [-1, 0 , 1]:
+                try:
+                    is_neighbor_alive = self.seed[y + i][x + j]
+                except:
+                    is_neighbor_alive = 0
+                neighbors += is_neighbor_alive
+
+        return neighbors
