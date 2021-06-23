c0d0f4a0fa369e3b8d92fc8b4ad7dd618b5e3074
lenshinoda <lebps1996@gmail.com>
2021-06-23 02:00:11 -0500

    [test] An exception should be raised if state is invalid - pass

--- game/cell.py
+++ game/cell.py
@@ -3,3 +3,13 @@
 class Cell:
     def __init__(self, state):
         self.state = state
+
+    @property
+    def state(self):
+        return self._state
+    
+    @state.setter
+    def state(self, state):
+        if not state in [0, 1]:
+            raise Exception("Invalid state")
+        self._state = state
