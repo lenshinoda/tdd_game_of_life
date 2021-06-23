16b1968843eba5cd7d3ef06e82486b90d17204de
lenshinoda <lebps1996@gmail.com>
2021-06-23 01:59:47 -0500

    [test] An exception should be raised if state is invalid - fail

--- tests/cell_test.py
+++ tests/cell_test.py
@@ -1,4 +1,5 @@
 from game.cell import Cell
+import pytest
 
 def test_cell_state_can_be_dead_or_alive():
     state = 0
@@ -8,3 +9,11 @@ def test_cell_state_can_be_dead_or_alive():
 
     assert dead_cell.state == 0
     assert alive_cell.state == 1
+
+def test_cell_should_raise_an_exception_if_invalid_state():
+    state = 3
+
+    with pytest.raises(Exception) as e_info:
+        Cell(state)
+    
+    assert e_info.value.args[0] == 'Invalid state'
