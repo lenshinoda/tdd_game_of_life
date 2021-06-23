4d0c6c1cff0f77eb48fdc46574f5c9b31d0aa036
lenshinoda <lebps1996@gmail.com>
2021-06-23 08:50:00 -0500

    [test-grid] Given a seed the next state should be calculated - fail

--- tests/grid_test.py
+++ tests/grid_test.py
@@ -35,3 +35,9 @@ def test_given_a_cell_location_the_neighbors_should_be_calculated():
     neighbors = grid.calculate_neighbors([8, 5])
 
     assert neighbors == 3
+
+def test_given_a_seed_the_next_state_should_be_calculated():
+    grid = Grid(seed)
+    grid.calculate_next_gen()
+
+    assert grid.seed == seed_next_generation
