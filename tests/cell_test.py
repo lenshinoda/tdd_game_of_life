7f549ab25a7a30ebe6b9fd954b281888d7cc8019
lenshinoda <lebps1996@gmail.com>
2021-06-23 02:14:26 -0500

    [test] Any live cell with fewer than two live neighbors dies, as if by underpopulation - fail

--- tests/cell_test.py
+++ tests/cell_test.py
@@ -17,3 +17,15 @@ def test_cell_should_raise_an_exception_if_invalid_state():
         Cell(state)
     
     assert e_info.value.args[0] == 'Invalid state'
+
+
+# Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+def test_a_live_cell_should_die_if_less_than_two_cells_are_alive():
+    live_cell_with_1_neighbor = Cell(1)
+    live_cell_with_2_neighbor = Cell(1)
+
+    live_cell_with_1_neighbor.update_state(0)
+    live_cell_with_2_neighbor.update_state(1)
+
+    assert live_cell_with_1_neighbor.state == 0
+    assert live_cell_with_2_neighbor.state == 0
